6fc48eb9894c92ef3508b7632612102c0ad130eb
Isaac <IsaacPasco@icloud.com>
2023-11-24 13:34:47 -0800

    modified:   NavNet.cpp 	modified:   NavNet.h

--- Source/Spacer/NavNet.cpp
+++ Source/Spacer/NavNet.cpp
@@ -2,13 +2,13 @@
 
 
 #include "NavNet.h"
-
+#include "Kismet/KismetSystemLibrary.h"
 
 // Sets default values
 ANavNet::ANavNet()
 {
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
-	PrimaryActorTick.bCanEverTick = true;
+	PrimaryActorTick.bCanEverTick = false;
 
 }
 
@@ -25,4 +25,21 @@ void ANavNet::Tick(float DeltaTime)
 	Super::Tick(DeltaTime);
 
 }
+bool ANavNet::BoxTrace(FVector Location, float sidelength){
+	FHitResult res;
+	return UKismetSystemLibrary::BoxTraceSingle(GetWorld(), Location,Location,(FVector){sidelength/2,sidelength/2,sidelength/2}, (FRotator){90,0,0}, ETraceTypeQuery::TraceTypeQuery1,false, {this},EDrawDebugTrace::None,res,false);
+}
+TArray<bool> ANavNet::GenerateVoxelGrid(){
+	TArray<bool> out;
+	return out;
 
+}
+TArray<FVector> ANavNet::GetNodeLocations(){
+		return {};
+}
+void ANavNet::RegenerateNet(){
+	TArray<bool> Voxels = GenerateVoxelGrid();
+}
+TArray<FVector> ANavNet::Pathfind(FVector Start, FVector End){
+	return {};
+}
--- Source/Spacer/NavNet.h
+++ Source/Spacer/NavNet.h
@@ -5,7 +5,11 @@
 #include "CoreMinimal.h"
 #include "GameFramework/Actor.h"
 #include "NavNet.generated.h"
-
+struct Node_t{
+	Node_t * Neighbors[16];
+	int NumNeighbors;
+	FVector Location;
+};
 UCLASS()
 class SPACER_API ANavNet : public AActor
 {
@@ -22,7 +26,18 @@ protected:
 public:	
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
-
-	
-	
+	TArray<Node_t> nodes;
+	UFUNCTION(BlueprintCallable)
+	TArray<FVector> GetNodeLocations();
+	UFUNCTION(BlueprintCallable)
+	void RegenerateNet();
+	UFUNCTION(BlueprintCallable)
+	TArray<FVector> Pathfind(FVector Start, FVector End);
+	UPROPERTY(BlueprintReadWrite,EditAnywhere)
+	FVector Bounds = {1000,1000,1000};
+	UPROPERTY(BlueprintReadWrite,EditAnywhere)
+	float VoxelSize = 100;
+private:
+	TArray<bool> GenerateVoxelGrid();
+	bool BoxTrace(FVector Location, float sidelength);
 };
